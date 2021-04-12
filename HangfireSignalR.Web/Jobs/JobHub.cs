21c2ceac367fc3d3e818658b3907cc29e16c9ce0
Scott Sauber <stsauber@gmail.com>
2021-04-11 22:52:58 -0500

    it works but its ugly

---
+++ HangfireSignalR.Web/Jobs/JobHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HangfireSignalR.Web.Jobs
+{
+    public class JobHub : Hub
+    {
+        private readonly IBackgroundJobClient _backgroundJobClient;
+        private readonly IThingDoerService _thingDoerService;
+
+        public JobHub(IBackgroundJobClient backgroundJobClient, IThingDoerService thingDoerService)
+        {
+            _backgroundJobClient = backgroundJobClient;
+            _thingDoerService = thingDoerService;
+        }
+        
+        public async Task StartJob(string storeCode)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, storeCode);
+            _backgroundJobClient.Enqueue(() => _thingDoerService.DoTheThings(storeCode));
+        }
+    }
+}
---
+++ HangfireSignalR.Web/Jobs/ThingDoerService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HangfireSignalR.Web.Jobs
+{
+    public interface IThingDoerService
+    {
+        Task DoTheThings(string storeCode);
+    }
+
+    public class ThingDoerService : IThingDoerService
+    {
+        private readonly IHubContext<JobHub> _hubContext;
+
+        public ThingDoerService(IHubContext<JobHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        
+        public async Task DoTheThings(string storeCode)
+        {
+            await _hubContext.Clients.Groups(storeCode).SendAsync("reportProgress", "Starting to do the things");
+            await Task.Delay(5000);
+            await _hubContext.Clients.Groups(storeCode).SendAsync("reportProgress", "Finished doing the things");
+        } 
+    }
+}
